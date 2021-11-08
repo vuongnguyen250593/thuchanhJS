dc5fab1953553a8bc004e20ca52e1e3065854835
Vương Nguyễn <vuongnqv@gmail.com>
2021-11-08 16:32:59 +0700

    them file thuc hanh su dung toan tu

---
+++ thuchanhsudungtoantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("inputWidth")
+inputHeight = prompt("inputHeight");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
